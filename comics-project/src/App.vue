0c6d9e7d601580337788c959425b3fea99d19b00
flaviovitale02 <flaviovitale73@gmail.com>
2023-06-20 17:51:57 +0200

    g

---
+++ comics-project/src/App.vue
@@ -0,0 +1,27 @@
+<script>
+
+import Header from "./components/AppHeader.vue"
+import Main from "./components/AppMain.vue"
+import Footer from "./components/AppFooter.vue"
+
+export default {
+  name: "App",
+
+  components:{
+    Header,
+    Main,
+    Footer
+  }
+}
+
+</script>
+
+<template>
+    <Header/>
+    <Main/>
+    <Footer/>
+</template>
+
+<style lang="scss" >
+  @import './styles/general.scss'
+</style>
